d7ed4764a1f02b6831515724ca7ce857ae5fa0f6
Francis Scipik Tang <fltang@ucsc.edu>
2014-06-04 03:32:33 -0700

    Cool Buttons
    Made some scrolls with animations as our buttons.
    Charles, if you could make/find a sounds for them it would be awesome.

---
+++ Qin's Conquest/Assets/Scripts/Menu/ButtonAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAnimation : MonoBehaviour {
+
+	public int level;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	void OnMouseOver() {
+		if(!CameraFade.fading) {
+			animator.SetBool("MouseOver", true);
+		}
+	}
+	
+	void OnMouseExit() {
+		if(!CameraFade.fading) {
+			animator.SetBool("MouseOver", false);
+		}
+	}
+	
+	void OnMouseUp() {
+		animator.SetBool("MouseOver", false);
+		if(!CameraFade.fading) {
+			CameraFade.StartAlphaFade( Color.black, false, 2f, 0.0f, loadLevelOnComplete);
+		}
+	}
+	
+	void loadLevelOnComplete() {
+		Application.LoadLevel (level);
+	}
+}
