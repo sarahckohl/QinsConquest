aae1b3c1f8d15a41fd5bf2fe1e0fcf6bee058a5c
Kenneth Thieu <kthieu@ucsc.edu>
2014-06-05 02:24:41 -0700

    Nothing
    Had to commit my incomplete progress to get Francis's work

--- Qin's Conquest/Assets/Scripts/Game/Misc/EnemyTargetModule.cs
+++ Qin's Conquest/Assets/Scripts/Game/Misc/EnemyTargetModule.cs
@@ -5,14 +5,7 @@ public class EnemyTargetModule : MonoBehaviour {
 	public static bool foundTarget = false;
 	public static int targetID = -1; //Target Player
 	public static int stopID = -1; //Hex Tile for Enemy Unit to aim for to be adjacent to the target Player
+	public static GameObject target;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
