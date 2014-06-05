72d6de18d96df4a444a002ebfc701de6030fa2c9
daitoshokan <gknoble@ucsc.edu>
2014-06-05 11:51:27 -0700

    Sexy changes
    Signed-off-by: daitoshokan <gknoble@ucsc.edu>

--- Qin's Conquest/Assets/Scripts/Menu/Button.cs
+++ Qin's Conquest/Assets/Scripts/Menu/Button.cs
@@ -7,12 +7,21 @@ public class Button : MonoBehaviour {
 	public int level;
 	public float widthOffset = 75, heightOffset = 50;
 	public float width = 150, height = 75;
+	public float xposition;
+	public float yposition;
 	// public Texture2D buttonSprite;
 	public string buttonText;
+
+	void Awake(){
+		xposition = gameObject.transform.position.x *300;
+		yposition = gameObject.transform.position.y *300;
+		buttonText = gameObject.name;
+	
+	}
 	
 	void OnGUI() {
 		GUI.depth = -5;
-		if (GUI.Button (new Rect (Screen.width / 2 - widthOffset, Screen.height / 2 + heightOffset, width, height), buttonText)) {
+		if (GUI.Button (new Rect (xposition, yposition, width, height), buttonText)) {
 			if(!CameraFade.fading) {
 				CameraFade.StartAlphaFade( Color.black, false, 2f, 0.0f, loadLevelOnComplete);
 			//	playButtonSound();
