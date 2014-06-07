ee7edb5c94d700e697ff4471c8a18171ecda7712
Francis Scipik Tang <fltang@ucsc.edu>
2014-06-07 01:23:06 -0700

    Level Editor Change
    Made buttons the territory itself, looks like shit.
    Removed the state scenes, we only need one game scene with this method

---
+++ Qin's Conquest/Assets/Scripts/Menu/LevelSelectButtons.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectButtons : MonoBehaviour {
+
+	public string level;
+	
+	public bool conquered = false;
+	
+	public Sprite original;
+	public Sprite highlight;
+	public Sprite takenOver;
+	
+	private SpriteRenderer rend;
+	
+	void Start() {
+		rend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rend.sprite != takenOver && conquered) {
+			rend.sprite = takenOver;
+		}
+	}
+	
+	void OnMouseOver() {
+		if(!CameraFade.fading && !conquered) {
+			rend.sprite = highlight;
+		}
+	}
+	
+	void OnMouseExit() {
+		if(!CameraFade.fading && !conquered) {
+			rend.sprite = original;
+		}
+	}
+	
+	void OnMouseUp() {
+		if(!CameraFade.fading && !conquered) {
+			rend.sprite = original;
+			GameState.level = level;
+			Application.LoadLevel (2);
+			// CameraFade.StartAlphaFade( Color.black, false, 2f, 0.0f, loadLevelOnComplete);
+		}
+	}
+	
+	void loadLevelOnComplete() {
+		Application.LoadLevel (level);
+	}
+}
