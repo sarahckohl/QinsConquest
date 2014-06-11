be55c1517bb9e789d4dcc46647bb81e04f59bf50
Francis Scipik Tang <fltang@ucsc.edu>
2014-06-10 20:21:11 -0700

    Selected Unit UI
    Have UI for selecte units now along with showing enemy unit range when selected
    Also the long horizontal scroll has an animation now

---
+++ Qin's Conquest/Assets/Scripts/Game/Misc/SelectionStats.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionStats : MonoBehaviour {
+
+	public SpriteRenderer unitSprite;
+	
+	public GUIText health;
+	public GUIText attack;
+	public GUIText defense;
+	public GUIText movement;
+	public GUIText attackRange;
+	
+	private Animator animator;
+	
+	
+	void Start() {
+		animator = this.GetComponent<Animator>();
+		deSelection ();
+	}
+	
+	public void setSelection(GameObject uni) {
+		StartCoroutine(setSelectionCoroutine(uni));
+	}
+	
+	public void deSelection() {
+		unitSprite.sprite = null;
+		health.text = "";
+		attack.text = "";
+		defense.text = "";
+		movement.text = "";
+		attackRange.text = "";
+		animator.SetBool("Close", true);
+	}
+	
+	IEnumerator setSelectionCoroutine(GameObject uni) {
+		animator.SetBool("Close", false);
+		yield return new WaitForSeconds(0.4f);
+		
+		// A check to see if player deselected unit while we were yielding
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("Scroll_Open")) {
+			unitSprite.sprite = uni.GetComponent<SpriteRenderer>().sprite;
+			if (uni.GetComponent<Unit>() != null) 
+				settingUnit (uni.GetComponent<Unit>());
+			else if (uni.GetComponent<Structure>() != null)
+				settingStruct(uni.GetComponent<Structure>());
+			else 
+				Debug.Log ("Shits Broken");
+		}
+	}
+	
+	void settingUnit(Unit unit) {
+		health.text = "Health: " + unit.health;
+		attack.text = "Attack: " + unit.attackVal;
+		defense.text = "Defense: " + unit.defenseVal;
+		movement.text = "Movement:" + unit.movement;
+		attackRange.text = "Range: " + unit.attackRange;
+	}
+	
+	void settingStruct(Structure struc) {
+		health.text = "Health: " + struc.health;
+	}
+}
