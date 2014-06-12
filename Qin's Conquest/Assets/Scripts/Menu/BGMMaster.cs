d7b2170c83d20635b6b7e2dcfe3b880de63a21dd
Francis Scipik Tang <fltang@ucsc.edu>
2014-06-11 22:48:52 -0700

    Title music
    Title musics in, battle music is now in BGM gameobjectcoe at start screen

---
+++ Qin's Conquest/Assets/Scripts/Menu/BGMMaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMMaster : MonoBehaviour {
+
+	public AudioClip mainMusic;
+	public AudioClip battleMusic;
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+		mainMusic = Resources.Load <AudioClip>("Audio/BGM/TitleTheme");
+		battleMusic = Resources.Load<AudioClip>("Audio/BGM/Battle");
+		audio.loop = true;
+		audio.clip = mainMusic;
+		audio.Play();
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		if (level == 2) {
+			audio.Stop();
+			audio.clip = battleMusic;
+			audio.Play();
+		} else {
+			if (audio.clip != mainMusic) {
+				audio.Stop();
+				audio.clip = mainMusic;
+				audio.Play();
+			}
+		}
+	}
+}
